92e3e9f9d841ccec7a626c1354d13c526bac254b
alka62 <alkak9169@gmail.com>
2026-02-15 22:45:46 +0530

    Created using Colab

---
+++ Personal_intro.py
@@ -0,0 +1,78 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyP+4gnU/RZJMScIl8lpAZ9y",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/alka62/Personal_introduction_program/blob/main/Personal_intro.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "J6uiJcbGRQ_E"
+      },
+      "outputs": [],
+      "source": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# Personal Introduction Program\n",
+        "\n",
+        "# Taking user input\n",
+        "name = input(\"What is your name? \")\n",
+        "age = input(\"How old are you? \")\n",
+        "hobby = input(\"What is your favorite hobby? \")\n",
+        "\n",
+        "# Displaying welcome message\n",
+        "print(\"\\n🎉 Welcome\", name, \"🎉\")\n",
+        "print(\"You are\", age, \"years old and love\", hobby + \".\")\n",
+        "print(\"Nice to meet you! 😊\")\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "bWdvCaSmRWF3",
+        "outputId": "039de093-61bc-41d7-a1b2-3966afb2a62d"
+      },
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "What is your name? Alka\n",
+            "How old are you? 21\n",
+            "What is your favorite hobby? Coding\n",
+            "\n",
+            "🎉 Welcome Alka 🎉\n",
+            "You are 21 years old and love Coding.\n",
+            "Nice to meet you! 😊\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
